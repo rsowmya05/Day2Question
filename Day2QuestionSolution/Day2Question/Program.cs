3fa1cfb9b5896f585627ede58b18147a7b2d2866
Sowmya <sowmya.ramesh@lntinfotech.com>
2020-12-16 16:40:14 +0530

    Prime Numbers

---
+++ Day2QuestionSolution/Day2Question/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Question
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min, max, i, j, flag;      
+            
+            Console.WriteLine("Enter min value: ");
+            min = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter max value: ");
+            max = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPrime numbers between {0} and {1} : ", min, max);
+            for (i = min; i <= max; i++)
+            {
+                if (i == 1 || i == 0)
+                    continue;
+                flag = 1;
+
+                for (j = 2; j <= i / 2; ++j)
+                {
+                    if (i % j == 0)
+                    {
+                        flag = 0;
+                        break;
+                    }
+                }
+                if (flag == 1)
+                {
+                    Console.WriteLine(i);
+                }
+                    
+            }
+            Console.ReadKey();
+        }
+        
+    }
+    
+}
---
+++ Day2QuestionSolution/Day2Question/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Day2Question")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Day2Question")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("064f1f4d-db9b-41fa-a7bb-24adb2a9b93f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
